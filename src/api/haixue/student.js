281661c552ba393a67cbe0e018d229a0ffaf2463
王金石 <jinshi.wang@ly.com>
2020-02-21 01:59:34 +0800

    第一次提交

---
+++ src/api/haixue/student.js
@@ -0,0 +1,26 @@
+import request from '@/utils/request'
+
+export function add(data) {
+  return request({
+    url: 'api/students',
+    method: 'post',
+    data
+  })
+}
+
+export function del(ids) {
+  return request({
+    url: 'api/students',
+    method: 'delete',
+    data: ids
+  })
+}
+
+export function edit(data) {
+  return request({
+    url: 'api/students',
+    method: 'put',
+    data
+  })
+}
+export default { add, edit, del }
