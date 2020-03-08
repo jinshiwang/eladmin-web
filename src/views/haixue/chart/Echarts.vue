435876a3e4cbb91321e00d4a1df4249a3ceef25e
王金石 <jinshi.wang@ly.com>
2020-03-08 17:41:59 +0800

    添加表表 表格

--- src/views/haixue/chart/Echarts.vue
+++ src/views/haixue/chart/Echarts.vue
@@ -6,6 +6,31 @@
           <category />
         </div>
       </el-row>
+      <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
+        <div class="chart-wrapper">
+          <el-table
+            :data="tableData"
+            height="250"
+            border
+            style="width: 100%"
+          >
+            <el-table-column
+              prop="pyear"
+              label="考研年份"
+              width="180"
+            />
+            <el-table-column
+              prop="total"
+              label="学生数量（个）"
+              width="180"
+            />
+            <el-table-column
+              prop="amount"
+              label="总输入金额（元）"
+            />
+          </el-table>
+        </div>
+      </el-row>
     </div>
   </div>
 </template>
@@ -13,11 +38,22 @@
 <script>
 
 import Category from '@/components/Echarts/StudentChart'
+import studnetchart from '@/api/haixue/student'
 
 export default {
   name: 'Echarts',
   components: {
     Category
+  },
+  data() {
+    return {
+      tableData: []
+    }
+  },
+  mounted() {
+    studnetchart.chart().then(res => {
+      this.tableData = res.groups
+    })
   }
 }
 </script>
