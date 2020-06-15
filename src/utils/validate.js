171ed13af70e0accae6247f0235cc117c3b998e2
王金石 <jinshi.wang@ly.com>
2020-06-15 15:00:56 +0800

    修改验证js

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -65,7 +65,7 @@ export function validEmail(email) {
 }
 
 export function isvalidPhone(phone) {
-  const reg = /^1[3|4|5|7|8][0-9]\d{8}$/
+  const reg = /^1[3|4|5|7|8|9][0-9]\d{8}$/
   return reg.test(phone)
 }
 
